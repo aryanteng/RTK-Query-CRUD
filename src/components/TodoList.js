f687966cf959195ea07ce03fa781671717bf044c
aryanteng <aryanteng892@gmail.com>
2022-12-06 02:45:05 +0530

    app

---
+++ src/components/TodoList.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faUpload } from "@fortawesome/free-solid-svg-icons";
+import {
+  useAddTodoMutation,
+  useDeleteTodoMutation,
+  useGetTodosQuery,
+  useUpdateTodoMutation,
+} from "../features/api/apiSlice";
+
+function TodoList() {
+  const [newTodo, setNewTodo] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo({ userId: 1, title: newTodo, completed: false });
+    setNewTodo("");
+  };
+
+  const newItemSection = (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="new-todo">Enter a new todo item</label>
+      <div className="new-todo">
+        <input
+          type="text"
+          id="new-todo"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Enter new todo"
+        />
+      </div>
+      <button className="submit">
+        <FontAwesomeIcon icon={faUpload} />
+      </button>
+    </form>
+  );
+
+  var content;
+
+  const {
+    data: todos,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetTodosQuery();
+  const [addTodo] = useAddTodoMutation();
+  const [updateTodo] = useUpdateTodoMutation();
+  const [deleteTodo] = useDeleteTodoMutation();
+
+  if (isLoading) {
+    return <>Loading...</>;
+  } else if (isSuccess) {
+    content = todos.map((todo) => {
+      return (
+        <article key={todo.id}>
+          <div className="todo">
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              id={todo.id}
+              onChange={() =>
+                updateTodo({ ...todo, completed: !todo.completed })
+              }
+            />
+            <label htmlFor={todo.id}>{todo.title}</label>
+          </div>
+          <button className="trash" onClick={() => deleteTodo({ id: todo.id })}>
+            <FontAwesomeIcon icon={faTrash} />
+          </button>
+        </article>
+      );
+    });
+  } else if (isError) {
+    content = <p>{error}</p>;
+  }
+
+  return (
+    <>
+      <h1>Todo List</h1>
+      {newItemSection}
+      {content}
+    </>
+  );
+}
+
+export default TodoList;
